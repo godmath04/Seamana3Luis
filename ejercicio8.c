53ba1a2a3de35cda9485e6fc9f7699609153b5d5
Luis Pineda <luis.pineda.revelo@udla.edu.ec>
2023-10-17 13:37:46 -0500

    Se completo el codigo entre el do y el while para llevar a cabo lo requerido por el problema

--- ejercicio8.c
+++ ejercicio8.c
@@ -3,6 +3,19 @@
 int main() {
     int minimo = 2;
     int maximo = 200;
-    printf("EL PROGRAMA MOSTRAR LOS NUMEROS PARES ENTRE 20 Y 200");
+    int x=2;
+    int pares;
+    printf("EL PROGRAMA MOSTRAR LOS NUMEROS PARES ENTRE 20 Y 200: \n");
+    do
+    {
+        pares = x%2;
+        if (pares == 0)
+        {
+           printf("Numero par: %d\n", x);
+        }
+        x++;
+    } while (x<=200 && x>=2);
+    
+    
     return 0;
 }
