d870f06dea9589bd572c4778093450eca286684b
Luis Pineda <luis.pineda.revelo@udla.edu.ec>
2023-10-17 13:24:18 -0500

    Declaracion de variables fijas y descripcion del programa

---
+++ ejercicio8.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    int numero;
+    printf("Por favor, ingrese un número:\n");
+    scanf("%d", &numero);
+    printf("Su número ingresado es: %d\n", numero);
+    return 0;
+}
