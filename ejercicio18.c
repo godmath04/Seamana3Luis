95d181dd39d1884eb5d70e0cb2654571d3bf85fe
Luis Pineda <luis.pineda.revelo@udla.edu.ec>
2023-10-17 18:39:30 -0500

    Variables faltantes, introduccion del do while para los multiplos

--- ejercicio18.c
+++ ejercicio18.c
@@ -2,7 +2,7 @@
 
 int menor, mayor;
 int MULTIPLO = 5;
-
+int x;
 int main()
 {
     printf("PROGRAMA QUE LEE DOS NUMEROS Y MUESTRA LOS MULTIPLOS DE 5 ENTRE ELLOS: \n");
@@ -10,5 +10,17 @@ int main()
     printf("\n");
     scanf("%d", &mayor);
     printf("Sus numeros son %d y %d", menor, mayor);
+    x = menor;
+    do
+    {
+        if (x % MULTIPLO == 0)
+        {
+            printf("%d", &x);
+        }
+        
+        
+    } while (x>=menor && x<=mayor);
+    
+    
     return 0;
 }
