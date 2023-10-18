22e892a91bc8b1e6badb6bdb41e36590b520678e
Luis Pineda <luis.pineda.revelo@udla.edu.ec>
2023-10-17 19:26:34 -0500

    Programa recibe los datos correctamente y los muestra

---
+++ ejercicio28.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int num1, num2;
+int cont1 = 0;
+int cont2 = 0;
+int main()
+{
+    printf("VERIFICAR EL NUMERO CON MAYOR CANTIDAD DE NUMEROS PRIMOS\n");
+    printf("Ingrese dos numeros: \n");
+    scanf("%d %d", &num1, &num2);
+    printf("Los numeros ingresados son: %d y %d", num1, num2);
+    return 0;
+}
