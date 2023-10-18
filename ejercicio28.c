b5490c6610bc7f4f96b4ebf721d1453ce851d22d
Luis Pineda <luis.pineda.revelo@udla.edu.ec>
2023-10-17 21:27:39 -0500

    Se separo la verificacion de primo en otra funcion para que sea m'as facil trabajar en main y hacer un uso mejor

--- ejercicio28.c
+++ ejercicio28.c
@@ -1,37 +1,40 @@
 #include<stdio.h>
-int num1, num2;
+int num1, num2, user1, user2;
 int cont1 = 0;
 int cont2 = 0;
-int main()
-{
-    printf("VERIFICAR EL NUMERO CON MAYOR CANTIDAD DE NUMEROS PRIMOS\n");
-    printf("Ingrese dos numeros: \n");
-    scanf("%d %d", &num1, &num2);
-    printf("Los numeros ingresados son: %d y %d\n", num1, num2);
+int primo1 = 0;
+int primo2 = 0;
+
+void primos(int num1, int num2){
 
     // Bucle para verificar si un numero es primo
     for (int i = 1; i <= num1; i++){   
         if (num1 % i == 0){
-            cont1++;
+            primo1++;
         }     
     }
-    if (cont1 == 2){
-        printf("Es primo");
-    }else{
-        printf("No es primo");
-         }
+    if (primo1 == 2){
+        cont1++;
+    }
             
 
     for (int j = 1; j <= num2; j++){   
         if (num2 % j == 0){
-            cont2++;
+            primo2++;
         }     
     }
-    if (cont2 == 2){
-        printf("Es primo");
-    }else{
-        printf("No es primo");
-         }
+    if (primo2 == 2){
+        cont2++;
+    }
+}
+
+int main()
+{
+    printf("VERIFICAR EL NUMERO CON MAYOR CANTIDAD DE NUMEROS PRIMOS\n");
+    printf("Ingrese dos numeros: \n");
+    scanf("%d %d", &user1, &user2);
+    printf("Los numeros ingresados son: %d y %d\n", user1, user2);
+    primos(user1, user2);
 
     return 0;
 }
