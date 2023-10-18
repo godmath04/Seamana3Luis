db2d119c979a5f83ad883aaaeefcf7432d3d1086
Luis Pineda <luis.pineda.revelo@udla.edu.ec>
2023-10-17 19:54:04 -0500

    Se agrego los if para verificar que los numeros sean primos, no es lo solicitado pero ayudara despues

--- ejercicio28.c
+++ ejercicio28.c
@@ -7,6 +7,31 @@ int main()
     printf("VERIFICAR EL NUMERO CON MAYOR CANTIDAD DE NUMEROS PRIMOS\n");
     printf("Ingrese dos numeros: \n");
     scanf("%d %d", &num1, &num2);
-    printf("Los numeros ingresados son: %d y %d", num1, num2);
+    printf("Los numeros ingresados son: %d y %d\n", num1, num2);
+
+    // Bucle para verificar si un numero es primo
+    for (int i = 1; i <= num1; i++){   
+        if (num1 % i == 0){
+            cont1++;
+        }     
+    }
+    if (cont1 == 2){
+        printf("Es primo");
+    }else{
+        printf("No es primo");
+         }
+            
+
+    for (int j = 1; j <= num2; j++){   
+        if (num2 % j == 0){
+            cont2++;
+        }     
+    }
+    if (cont2 == 2){
+        printf("Es primo");
+    }else{
+        printf("No es primo");
+         }
+
     return 0;
 }
